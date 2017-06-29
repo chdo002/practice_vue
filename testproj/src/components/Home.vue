c0cfbf79b68612d472b214f701b81941f3ba2f8f
chdo <chdo@chdodeMacBook-Pro.local>
2017-06-29 23:07:37 +0800

    init

---
+++ testproj/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template id="Home">
+  <div class="">
+    <p>1233</p>
+    <cell></cell>
+  </div>
+
+</template>
+
+<script>
+
+import cell from '@/components/cell'
+
+export default {
+  components: {
+    cell
+  }
+}
+</script>
+
+<style type="text/css">
+
+</style>
---
+++ testproj/src/components/cell.vue
@@ -0,0 +1,75 @@
+<template id="cell">
+	
+	<div class="cellBack"  >
+		<img id='thum' src="../assets/123.png" alt="../assets/123.png">
+	</div>
+
+
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<style type="text/css">
+	.cellBack{
+		background-color: grey;
+		width: 100%;
+		height: 100px;
+	}
+	#thum{
+		vertical-align: middle;
+		display: block;
+		width: 50px;
+		height: 50px;
+		float: left;
+	}
+</style>
+
+
+<style type="text/css">
+	#list{
+		list-style: none;
+		width: 100%;
+		height: auto;
+		overflow: hidden;
+
+	}
+	.one{
+		width: 100%;
+		height: 2rem;
+		border-bottom: 1px solid #000000;
+
+	}
+	.userImage{
+				width: 1.4rem;
+				height: 1.4rem;
+				display: block;
+				float: left;
+				margin-top: 0.3rem;
+
+	}
+	.detail{
+		position: relative;
+		float: left;
+		width: 80%;
+		height: 100%;
+	}
+	.name{
+		font-size: .8rem;
+			color: #000000;
+			height: 1rem;
+			text-align: left;
+			margin: 0 0 0 2%;
+			line-height: 1rem;
+	}
+	.subTitle{
+		font-size: .8rem;
+			color: #000000;
+			height: 1rem;
+			text-align: left;
+			margin: 0 0 0 2%;
+			line-height: 1rem;
+	}
+
+</style>
